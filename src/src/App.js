c7e660c605e15a978cf57fab1f2618ae4f54c940
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-19 22:59:24 +0200

    Cenetered element

--- src/src/App.js
+++ src/src/App.js
@@ -1,99 +1,32 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 
-export default function Height() {
-  //console.log(theme);
+export default function HorizontalCentering() {
   return (
-    <Box sx={{ height: 100, width: '100%' }}>
+    <div>
       <Box
         sx={{
-          height: '25%',
-          width: 120,
-          display: 'inline-block',
+          mx: 'auto',
+          //ml:"auto",
+          //mr:"auto",
+          width: 200,
           p: 1,
-          mx: 1,
+          my: 1,
           bgcolor: (theme) =>
-            {console.log(theme);
-              return theme.palette.mode === 'dark' ? '#101010' : 'grey.100'},
+            theme.palette.mode === 'dark' ? '#101010' : 'grey.50',
           color: (theme) =>
             theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
           border: '1px solid',
           borderColor: (theme) =>
             theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
           borderRadius: 2,
+          textAlign: 'center',
           fontSize: '0.875rem',
           fontWeight: '700',
-          textAlign: 'center',
         }}
       >
-        Height 25%
+        Centered element
       </Box>
-      <Box
-        sx={{
-          height: '50%',
-          width: 120,
-          display: 'inline-block',
-          p: 1,
-          mx: 1,
-          bgcolor: (theme) =>
-            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          border: '1px solid',
-          borderColor: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-          borderRadius: 2,
-          fontSize: '0.875rem',
-          fontWeight: '700',
-          textAlign: 'center',
-        }}
-      >
-        Height 50%
-      </Box>
-      <Box
-        sx={{
-          height: '75%',
-          width: 120,
-          display: 'inline-block',
-          p: 1,
-          mx: 1,
-          bgcolor: (theme) =>
-            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          border: '1px solid',
-          borderColor: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-          borderRadius: 2,
-          fontSize: '0.875rem',
-          fontWeight: '700',
-          textAlign: 'center',
-        }}
-      >
-        Height 75%
-      </Box>
-      <Box
-        sx={{
-          height: '100%',
-          width: 120,
-          display: 'inline-block',
-          p: 1,
-          mx: 1,
-          bgcolor: (theme) =>
-            theme.palette.mode === 'dark' ? '#101010' : 'grey.100',
-          color: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.300' : 'grey.800',
-          border: '1px solid',
-          borderColor: (theme) =>
-            theme.palette.mode === 'dark' ? 'grey.800' : 'grey.300',
-          borderRadius: 2,
-          fontSize: '0.875rem',
-          fontWeight: '700',
-          textAlign: 'center',
-        }}
-      >
-        Height 100%
-      </Box>
-    </Box>
+    </div>
   );
 }
