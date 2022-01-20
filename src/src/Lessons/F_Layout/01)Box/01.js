96f24c79ee7af0cf8176c7c2f2c6a146b13e23ce
Omar Magdy <omar.magdy.code@gmail.com>
2022-01-20 17:56:28 +0200

    Box2.js: created

--- src/src/Lessons/F_Layout/01)Box/01.js
+++ src/src/Lessons/F_Layout/01)Box/01.js
@@ -1,8 +1,11 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
+import Container from '@mui/material/Container';
 
 export default function BoxSx() {
   return (
+    <Container 
+    sx={{width:"100%", height:"100%"}}>
     <Box
     component="button"
       sx={{
@@ -12,10 +15,10 @@ export default function BoxSx() {
         '&:hover': {
           backgroundColor: 'red',
           opacity: [0.9, 0.8, 0.7],
-          
         },
       }}
     >Hello
       </Box>
+      </Container>
   );
 }
